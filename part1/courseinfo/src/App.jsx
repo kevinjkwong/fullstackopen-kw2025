bafd655972a95e0cf98ad7351fbb326a2622273d
Kevin Wong <kwong@Kevins-MacBook-Pro.local>
2025-11-13 11:27:54 -1000

    Complete ex1.2, refactor Content component

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -2,8 +2,16 @@ const Header = ({ course }) => {
   return <h1>{course}</h1>
 }
 
-const Content = ({ part, exercise }) => {
+const Part = ({ part, exercise }) => {
   return <p>{part} {exercise}</p>
+}
+
+const Content = ({ exercises }) => {
+  return <>
+    <Part part={exercises.part1} exercise={exercises.exercise1}/>
+    <Part part={exercises.part2} exercise={exercises.exercise2}/>
+    <Part part={exercises.part3} exercise={exercises.exercise3}/>
+  </>
 }
 
 const Total = ({ exerciseTotal }) => {
@@ -13,19 +21,19 @@ const Total = ({ exerciseTotal }) => {
 const App = () => {
 
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercise1 = 10
-  const part2 = 'Using props to pass data'
-  const exercise2 = 7
-  const part3 = 'State of a component'
-  const exercise3 = 14
+  const exercises = {
+    part1: 'Fundamentals of React',
+    exercise1: 10,
+    part2: 'Using props to pass data',
+    exercise2: 7,
+    part3: 'State of a component',
+    exercise3: 14
+  }
 
   return <div>
     <Header course={course} />
-    <Content part={part1} exercise={exercise1} />
-    <Content part={part2} exercise={exercise2} />
-    <Content part={part3} exercise={exercise3} />
-    <Total exerciseTotal={exercise1 + exercise2 + exercise3} />
+    <Content exercises={exercises} />
+    <Total exerciseTotal={exercises.exercise1 + exercises.exercise2 + exercises.exercise3} />
   </div>
 }
 
